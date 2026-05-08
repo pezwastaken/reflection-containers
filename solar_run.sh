52929ac06f3a2f71116518b9245c2d6359e09af5
Andrea Pezzuolo <andrea.pezzuolo@studenti.unipr.it>
2026-05-08 22:34:57 +0200

    run solar analysis

--- solar_run.sh
+++ solar_run.sh
@@ -1,13 +1,22 @@
-# java -jar tai-e-all-0.5.2.jar -cp ../../reflection-container/2/main.jar -m Main -java 8 -a "pta=cs:2-type;time-limit:60;reflection-inference:solar;dump:true"
-#
-#
-#
 # the actual solar command
 #
 # java -jar tai-e-all-0.5.2.jar \
-#   -acp ../../reflection-container/2/main.jar -cp ../../reflection-container/2 \
+#   -acp path/to/.jar -cp path/to/directory/with/.class \
 #   -m Main -java 8 \
 #   -a "pta=cs:2-type;reflection-inference:solar;distinguish-string-constants:reflection;handle-invokedynamic:true" \
 #   -a "cg=algorithm:pta;dump-call-edges:true;dump-methods:true"
+
+
+mkdir solar_results
+
+./compile_java_test.sh "$1" && \
+	sudo docker run --name solar --rm -it \
+	-v "$(pwd)/$1":/analysis_input \
+	-v "$(pwd)/solar_results":/tai-e/Tai-e/output \
+	solar-analysis -acp /analysis_input/main.jar \
+		-cp /analysis_input \
+		-m Main -java 8 \
+		-a "pta=cs:2-type;reflection-inference:solar;distinguish-string-constants:reflection;handle-invokedynamic:true" \
+		-a "cg=algorithm:pta;dump-call-edges:true;dump-methods:true"
 
 
